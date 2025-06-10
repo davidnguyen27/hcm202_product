38af03287928a74f0b146baf4a0ecf5bb4f5d0dd
davidnguyen27 <theanhngujen2702@gmail.com>
2025-06-10 23:04:57 +0700

    fix chatbot

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,2 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
+export const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
--- src/features/Chat/services/chat.service.ts
+++ src/features/Chat/services/chat.service.ts
@@ -1,11 +1,22 @@
-import { customFetch } from "@/api/customFetch";
-import type { ChatRequest, ChatResponse } from "../types/chat.type";
+import { API_KEY } from "@/constants";
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: API_KEY });
 
 export async function sendChatMessage(prompt: string) {
-  const body: ChatRequest = { prompt };
-
-  return await customFetch<ChatResponse>("/chat", {
-    method: "POST",
-    body: JSON.stringify(body),
-  });
+  try {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: prompt,
+    });
+    return {
+      success: true,
+      data: { message: response.text || "" },
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: error instanceof Error ? error.message : "Đã xảy ra lỗi không xác định",
+    };
+  }
 }
